b692a82c0b9396b6162a2721a5921c8457fd8724
Melanie Chan <melanie.chan@sjsu.edu>
2022-04-12 17:39:59 -0700

    retrieved post data when navigating to specific post url

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
 from flask import Flask, render_template
+import sqlite3
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# every post has its own link, made using its unique post_id
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
+def get_db_connection():
+    connection = sqlite3.connect('database.db')
+    connection.row_factory = sqlite3.Row
+    return connection
+
+# find a post from db given its post_id
+def get_post(post_id):
+    connection = get_db_connection()
+    post = connection.execute('SELECT * FROM posts WHERE id = ?',(post_id,)).fetchone()
+    connection.close()
+
+    # show 'Not Found' page if post doesn't exist
+    if post is None:
+        abort(404)
+    return post
