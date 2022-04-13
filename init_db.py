62361246f6a75cc7390201f16ca86fc38e92494d
Melanie Chan <melanie.chan@sjsu.edu>
2022-04-12 17:00:35 -0700

    set up database

---
+++ init_db.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db') # file created when ran
+
+# execute commands in SQL file that set up a database
+with open('schema.sql') as file:
+    connection.executescript(file.read())
+
+connection.commit()
+connection.close()
